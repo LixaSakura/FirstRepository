327a63b53621f10af73f2b7b8376e678b4d67129
Monster2408 <meoto2408@gmail.com>
2025-05-22 09:39:33 +0900

    up

--- src/main/java/xyz/ecsv/Main.java
+++ src/main/java/xyz/ecsv/Main.java
@@ -1,7 +1,12 @@
 package xyz.ecsv;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // コンストラクタの引数でアプリタイトルを設定
+        JFrame frame = new JFrame("デモアプリ");
+        // デスクトップに表示する画面のサイズ（幅,高さ）
+        frame.setSize(500,500);
     }
 }
